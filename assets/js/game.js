2293ff50be59bbf0c896f0add63072c208e64c96
Nicole Gile <nicolegile94@gmail.com>
2020-06-14 12:52:24 -0500

    added multiple enemies

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,13 +3,11 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roberto";
+var enemyNames = ["Roberto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
     // Alert users that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -62,7 +60,8 @@ var fight = function() {
   } else {
     window.alert("You need to pick a valid option. Try again!");
   }
-
 };
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
