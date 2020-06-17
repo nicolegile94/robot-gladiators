e64bcfb2a3ba5532de442732cdf19935ee491bed
Nicole Gile <nicolegile94@gmail.com>
2020-06-17 16:11:01 -0500

    fixed fight prompt

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,33 +4,48 @@ var randomNumber = function(min, max) {
   return value;
 };
 
+var fightOrSkip = function(){
+  //ask user if they'd like to fight or skip using function
+  var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+
+  //Conditional Recursive Function Call
+  if (promptFight === "" || promptFight === null) {
+    window.alert("You need to provide a valid answer! Please try again.");
+    return fightOrSkip();
+  }
+
+  //if user picks "skip" confirm and then stop the loop
+  promptFight = promptFight.toLowerCase();
+  if (promptFight === "skip") {
+    // confirm user wants to skip
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?")
+
+    //if yes (true), leave fight
+    if (confirmSkip) {
+    window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!")
+    //subtract money from playerMoney for skipping
+    playerInfo.money = playerInfo.money - 10;
+
+    //return true if user wants to leave
+    return true;
+    shop();
+    }
+  }
+}
+
 var fight = function(enemy) {
   while(enemy.health > 0 && playerInfo.health > 0){
-    // ask user if they'd like to fight or run
-    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.")
-    // if player choses to skip confirm and then stop the loop
-    if (promptFight === "skip" || promptFight === "SKIP") {
-      // confirm user wants to skip
-       var confirmSkip = window.confirm("Are you sure you'd like to quit?")
-
-      //if yes (true), leave fight
-      if (confirmSkip) {
-        window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!")
-        //subtract money from playerMoney for skipping
-        playerInfo.money = playerInfo.money - 10;
-        console.log("playerMoney", playerInfo.money);
-        break;
-      }
+    if (fightOrSkip()) {
+      //if true, leave fight be breaking loop
+      break;
     }
-
-    //remove enemy's health by subtracting the amount set in the playerAttack variable
     var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
     enemy.health = Math.max(0, enemy.health - damage);
     
     console.log(
       playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining."
     );
-
+  
     //check enemy's health
     if (enemy.health <=0) {
       window.alert(enemy.name + " has died!");
